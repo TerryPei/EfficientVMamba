186cce20ae7f5b2435984c45f109f02236053328
Terry <694073982@gmail.com>
2024-03-22 14:45:31 +1100

    fix the import bug in __init__.py

--- classification/lib/models/__init__.py
+++ classification/lib/models/__init__.py
@@ -7,11 +7,3 @@ try:
     _has_timm = True
 except ModuleNotFoundError:
     _has_timm = False
-
-if _has_timm:
-    # from . import lightvit
-    # from . import vim
-    from . import vmamba_mobile
-    # from . import vmamba_local
-    # from . import vmamba_local_search
-    # from . import vmamba_mobile_fusion
